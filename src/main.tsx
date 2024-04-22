0f9c37332578068163b670544384d517ec93834b
maianh041103 <nguyenmaianh041103@gmail.com>
2024-04-22 21:01:08 +0700

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from '@/redux/store';
 import App from './App';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
